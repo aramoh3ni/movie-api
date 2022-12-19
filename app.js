a1aa90e9a5682288a7fb67d8e82c805786f4a3f7
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-19 21:45:33 +0430

    Explaining the concept of Global and Window object inside of Node.

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-function sayHello(name) {
-    console.log("Hello, ", name)
-}
+// global Objects
+console.log(); 
 
-console.log(window)
-// sayHello("Alireza")
+// setTimeout(); // call function after a delay
+// clearTimeout(); // clear function after execute
+
+// setInterval(); 
+// clearInterval();
+
+// we can use them using in window object globaly in browser
+// but we don't have logic of window object in node then we 
+// we can use gloal insted to window.
+
+// we can use windows object using gloabl but
+global.console.log("Hello Wolrd")
+
+// if we create an object we can node use it and it return us undefine.
+var message = "";
+console.log(global.message) // return Undefined
+// because it scope inside of this file (app.js) and it is not 
+// globaly visitbal outside for all.
+
