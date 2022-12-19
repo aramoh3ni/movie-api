e9a7aec49c3ca56d0972d27f9c05b21ebaff1f2b
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-19 22:06:48 +0430

    Explaing the concept of module.exports when, why, and which methods should be exported.

---
+++ logger.js
@@ -0,0 +1,11 @@
+var url = 'http://mylogger.io/log';
+
+function log(message) {
+    // send an HTTP request
+}
+
+module.exports.log = log;
+// module.exports.url = url;
+// we create and variable of function with and name but exported
+// with defirend name
+module.exports.endPont = url;
