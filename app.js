8d174950bd3a7ad092e956c25277bb20780c4549
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-19 22:48:59 +0430

    Working with OS module.

--- app.js
+++ app.js
@@ -7,7 +7,16 @@
 
 // console.log(log("ALireza"));
 
-const path = require("path");
-const pathObj = path.parse(__dirname);
+// Take information about file path or dir path
+// const path = require("path");
+// const pathObj = path.parse(__dirname);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+// Take information about Operating System.
+const os = require("os");
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log("Total Memory: ", (totalMemory / 1024) * 100);
+console.log("Free Memory: ", freeMemory);
