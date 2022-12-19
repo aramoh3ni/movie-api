be3fa81414fd3dfecf4be30c34154450d7302c88
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-19 22:40:23 +0430

    Implementing path moduel -> using path __dirname, __filename

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const log = require('./logger');
+// const log = require('./logger');
 
 // console.log(logger);
 // it log the exports => {log: [Function: log], endPoint: "http://myloggger.io/log"}
 
 // console.log(logger.log("Alireza"))
 
-console.log(log("ALireza"));
+// console.log(log("ALireza"));
+
+const path = require("path");
+const pathObj = path.parse(__dirname);
+
+console.log(pathObj);
