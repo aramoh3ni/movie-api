23d14e1ec88b61a988826e9a50b94059f9919da8
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-20 19:03:16 +0430

    Working with Node Package Manager (npm)

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-const http = require("http");
+const _ = require("underscore");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello world Root");
-    res.end();
-  }
+const result = _.contains([1, 2, 3, 4], 4);
 
-  if (req.url === "/api/books") {
-    res.write(
-      JSON.stringify([{ id: 1, title: "How to Become Node Developer." }])
-    );
-
-    res.end();
-  }
-});
-
-// server.addListener('connection', (socket) => {
-//     console.log("New Connection...");
-// })
-
-server.listen(3000);
-
-console.log("Listen on port 3000");
+console.log(result);
