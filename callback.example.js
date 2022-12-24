c34ee6b5eee0355f2a503c7e789a2ebea434b348
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-24 23:06:17 +0430

    replacing callback function to promises.

--- callback.example.js
+++ callback.example.js
@@ -10,23 +10,29 @@ function displayCommits(commits) {
   getCommits(commits);
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading User from Database...");
-    callback({ id: id, gitHubUsername: "aramoh3ni" });
-  }, 1000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading User from Database...");
+      resolve({ id: id, gitHubUsername: "aramoh3ni" });
+    }, 1000);
+  });
 }
 
-function getRepository(username, callback) {
-  setTimeout(() => {
-    console.log("Connection to Github.com...");
-    callback(["rep1", "rep2", "rep3"]);
-  }, 1000);
+function getRepository(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Connection to Github.com...");
+      resolve(["rep1", "rep2", "rep3"]);
+    }, 1000);
+  });
 }
 
-function getCommits(repos, callback) {
-  setTimeout(() => {
-    console.log("Reading commit from Github.com...");
-    callback(["commit1", "commit2", "commit3"]);
-  }, 1000);
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading commit from Github.com...");
+      resolve(["commit1", "commit2", "commit3"]);
+    }, 1000);
+  });
 }
