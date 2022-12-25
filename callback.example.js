172e19e28f401edc1893d6fd9c2c7544037bbcd5
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-25 11:58:03 +0430

    Implementing all three approch for asyncronouse operations (callback, promise, async/await)

--- callback.example.js
+++ callback.example.js
@@ -1,14 +1,35 @@
+// Call-Back Approch
 console.log("Before");
-getUser(1, getRepository);
+getUser(1, (user) => {
+  getRepository(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
+  });
+});
 console.log("After");
 
-// Flat Structure and chain them 
+// Promise-based Approach
+// Flat Structure and chain them
 getUser(1)
   .then((user) => getRepository(user.gitHubUsername))
   .then((repos) => getCommits(repos[0]))
   .then((commits) => console.log(commits))
   .catch((err) => console.log(err.message));
 
+// Async and Await Approch
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepository(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+displayCommits();
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
@@ -32,7 +53,8 @@ function getCommits(repos) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Reading commits from Github.com...");
-      resolve(["commit1", "commit2", "commit3"]);
+      // resolve(["commit1", "commit2", "commit3"]);
+      reject(new Error("Can't get Commits..."));
     }, 1000);
   });
 }
