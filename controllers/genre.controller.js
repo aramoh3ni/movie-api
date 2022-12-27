03bf3603ae56061050e33241651b8ae0fe97b67c
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-27 21:36:58 +0430

    Update genre model work more efficiant.

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -38,7 +38,7 @@ const setGenre = async (req, res) => {
     const result = await genre.save();
     if (!result) return res.status(400).send("Item not Insert");
 
-    res.status(200).send("Insert Succesfully");
+    res.status(201).send("Insert Succesfully");
   } catch (err) {
     res.status(400).send(err.message);
   }
@@ -49,19 +49,15 @@ const updateGenre = async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genreExists = await GenreModel.findOne({
-      _id: id,
-      name: req.body.name,
-    });
-    if (!genreExists)
-      return res.status(404).send("The genre with the given ID was not found.");
-
-    const result = await GenreModel.findByIdAndUpdate(
+    const genre = await GenreModel.findByIdAndUpdate(
       id,
       { name: req.body.name },
       { new: true }
     );
-    if (!result) return res.status(400).send("Update faild.");
+
+    if (!genre)
+      return res.status(404).send("The genre with the given ID was not found.");
+
     res.status(201).send("Update Succesfully");
   } catch (err) {
     res.status(400).send(err.message);
@@ -69,10 +65,11 @@ const updateGenre = async (req, res) => {
 };
 
 const deleteGenre = async (req, res) => {
-  const { id } = req.params;
   try {
-    const genre = await GenreModel.findByIdAndDelete(id);
-    if (!genre) return res.status(404).send("The genre with the given ID was not found.");
+    const { id } = req.params;
+    const genre = await GenreModel.findByIdAndRemove(id);
+    if (!genre)
+      return res.status(404).send("The genre with the given ID was not found.");
 
     res.status(200).send("Genre Deleted Successfully.");
   } catch (err) {
