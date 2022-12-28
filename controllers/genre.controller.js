c3d9cb121e62cdc8af5032aa5f0eaacff819afc9
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2022-12-28 22:46:32 +0430

    CODE REFACTORING genre controller

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -1,14 +1,15 @@
 // MODEL & VALIDATION
 const { GenreModel, validateGenre } = require("../models/genre.model");
-const { MovieModel } = require("../models/movie.model");
+// const { MovieModel } = require("../models/movie.model");
 
 const getGenre = async (req, res) => {
   try {
     const genres = await GenreModel.find().sort({ name: 1 });
-    if (!genres) return res.status(404).send("No Value.");
-    res.status(200).send(genres);
+    return !genres
+      ? res.status(404).json("No Value.")
+      : res.status(200).json(genres);
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(400).json(err.message);
   }
 };
 
@@ -16,8 +17,9 @@ const getGenreById = async (req, res) => {
   const { id } = req.params;
   try {
     const genre = await GenreModel.findById(id);
-    if (!genre) return res.status(404).send("Not Found!");
-    res.status(200).send(genre);
+    return !genre
+      ? res.status(404).json("Not Found!")
+      : res.status(200).json(genre);
   } catch (err) {
     res.status(400).json(err.message);
   }
@@ -26,28 +28,28 @@ const getGenreById = async (req, res) => {
 const setGenre = async (req, res) => {
   try {
     const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).json(error.details[0].message);
 
     const genreExists = await GenreModel.findOne({ name: req.body.name });
-    if (genreExists) return res.status(400).send("Item Exists");
+    if (genreExists) return res.status(400).json("Item Exists");
 
     const genre = new GenreModel({
       name: req.body.name,
     });
 
     const result = await genre.save();
-    if (!result) return res.status(400).send("Item not Insert");
-
-    res.status(201).send("Insert Succesfully");
+    return !result
+      ? res.status(400).json("Item not Insert")
+      : res.status(201).json(result);
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(400).json(err.message);
   }
 };
 const updateGenre = async (req, res) => {
   try {
     const { id } = req.params;
     const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).json(error.details[0].message);
 
     const genre = await GenreModel.findByIdAndUpdate(
       id,
@@ -55,12 +57,11 @@ const updateGenre = async (req, res) => {
       { new: true }
     );
 
-    if (!genre)
-      return res.status(404).send("The genre with the given ID was not found.");
-
-    res.status(201).send("Update Succesfully");
+    return !genre
+      ? res.status(404).json("The genre with the given ID was not found.")
+      : res.status(201).json(genre);
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(400).json(err.message);
   }
 };
 
@@ -69,11 +70,11 @@ const deleteGenre = async (req, res) => {
     const { id } = req.params;
     const genre = await GenreModel.findByIdAndRemove(id);
     if (!genre)
-      return res.status(404).send("The genre with the given ID was not found.");
+      return res.status(404).json("The genre with the given ID was not found.");
 
-    res.status(200).send("Genre Deleted Successfully.");
+    res.status(200).json("Genre Deleted Successfully.");
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(400).json(err.message);
   }
 };
 
