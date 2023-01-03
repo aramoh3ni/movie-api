ee3fe59c130cbe592326eefedb50ba9050449549
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-03 23:03:18 +0430

    exporting routes in single place.

---
+++ Routes/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  auth: require("./auth"),
+  customers: require("./customer"),
+  genres: require("./genre"),
+  movies: require("./movie"),
+  rentals: require("./rental"),
+  users: require("./users"),
+  main: require("./main"),
+};
--- startup/routes.js
+++ startup/routes.js
@@ -1,21 +1,28 @@
 const express = require("express");
-const auth = require("../routes/auth");
-const users = require("../routes/users");
-const movies = require("../routes/movie");
-const genres = require("../routes/genre");
-const customers = require("../routes/customer");
-const books = require("../routes/books");
-const rentals = require("../routes/rental");
-const home = require("../routes/main");
+const {
+  auth,
+  users,
+  movies,
+  genres,
+  customers,
+  rentals,
+  main,
+} = require("../routes");
+const { error } = require("../middleware");
 
 module.exports = function (app) {
+  // Parse to Json
   app.use(express.json());
+
+  // Routes
   app.use("/api/auth", auth);
   app.use("/api/users", users);
   app.use("/api/movies", movies);
-  app.use("/api/books", books);
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/rentals", rentals);
-  app.use("/", home);
+  app.use("/", main);
+
+  // Error Handler
+  app.use(error);
 };
