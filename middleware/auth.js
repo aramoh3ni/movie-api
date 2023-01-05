a86043af3665e2328bcc6f07fefd647930a9acfb
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-05 17:28:10 +0430

    TESTING - geres - POST is passed.

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,13 @@ const jwt = require("jsonwebtoken");
 
 const { authorization_msgs, auth_token_msgs } = require("../constants/message");
 
-module.exports = function auth(req, res, next) {
-  try {
-    const token = req.header("x-auth-token");
-    if (!token)
-      throw createError.Unauthorized(authorization_msgs.access_denied);
+module.exports = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) throw createError.Unauthorized(authorization_msgs.access_denied);
 
-    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
-    if (!payload) throw createError.Unauthorized(auth_token_msgs.invalid);
-    req.user = payload;
-    next();
-  } catch (error) {
-    res.status(500).json(auth_token_msgs.invalid);
-  }
+  const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
+  if (!payload)
+    throw createError.Unauthorized(authorization_msgs.access_denied);
+  req.user = payload;
+  next();
 };
--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,8 @@
-const c = require("config");
 const request = require("supertest");
 const { GenreModel } = require("../../models/genre.model");
+const { UserModel } = require("../../models/users.model");
+const mongoose = require("mongoose");
+
 let server;
 
 describe("/api/genres", () => {
@@ -11,6 +13,7 @@ describe("/api/genres", () => {
     server.close();
     await GenreModel.deleteMany({});
   });
+
   describe("GET /", () => {
     it("shoud return all genres", async () => {
       await GenreModel.insertMany([{ name: "genre1" }, { name: "genre2" }]);
@@ -22,11 +25,83 @@ describe("/api/genres", () => {
       expect(res.body.data.some((g) => g.name === "genre2")).toBe(true);
     });
   });
+
   describe("GET /:id", () => {
     it("should return 404 if invalid id is passed.", async () => {
       const res = await request(server).get(`/api/genres/1`);
       expect(res.status).toBe(404);
     });
-    
+  });
+
+  describe("POST /", () => {
+    it("should return 401 unuthorized if client in not logged in.", async () => {
+      const res = await request(server)
+        .post("/api/genres")
+        .send({ name: "genre1" });
+
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre property name lower then 5 characters.", async () => {
+      const user = {
+        _id: mongoose.Types.ObjectId().toHexString(),
+        isAdmin: true,
+      };
+      const token = new UserModel(user).genAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "123" });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if gerne property name is bigger then 50 characters.", async () => {
+      const user = {
+        _id: mongoose.Types.ObjectId().toHexString(),
+        isAdmin: true,
+      };
+      const token = new UserModel(user).genAuthToken();
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: (10 ^ 100).toString() });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should save genre if gerne is valid.", async () => {
+      const user = {
+        _id: mongoose.Types.ObjectId().toHexString(),
+        isAdmin: true,
+      };
+      const token = new UserModel(user).genAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "genre1" });
+
+      const genre = await GenreModel.find({ name: "genre1" });
+
+      expect(genre).not.toBeNull();
+    });
+
+    it("should save genre if gerne have all valid properties.", async () => {
+      const user = {
+        _id: mongoose.Types.ObjectId().toHexString(),
+        isAdmin: true,
+      };
+      const token = new UserModel(user).genAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "genre1" });
+
+      expect(res.body.data).toHaveProperty('name', "genre1");
+      expect(res.body.data).toHaveProperty("_id");
+    });
   });
 });
