da6b64c5e746d050508957864b9442eef2359cfe
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-10 23:22:52 +0430

    replace static messages with dynamic message.

--- controllers/returns.controller.js
+++ controllers/returns.controller.js
@@ -1,20 +1,10 @@
-// :: TODO
-// :: DONE => /api/rentals/return
-// :: DONE => Return 401 if client is not logged in.
-// :: DONE => Return 400 if customerId is not provided
-// :: DONE => Return 400 if movieid is not Provided
-// :: DONE => Return 404 if no rental found for this customer
-// :: DONE => Return 400 if rental is aleardy proccessed
-// :: DONE => Return 200 if valid request
-// :: DOBE => Set the return date
-// :: DONE => Calculate the rental fee
-// increase number in the stock
-// Return renatal object to the client.
 const moment = require("moment");
 const createError = require("http-errors");
 
 const { MovieModel } = require("../models/movie.model");
 const { RentalModel } = require("../models/rental.model");
+
+const { returns_msgs } = require("../constants/message");
 
 const setReturns = async (req, res) => {
   const rental = await RentalModel.findOne({
@@ -22,13 +12,9 @@ const setReturns = async (req, res) => {
     "movie._id": req.body.movieId,
   });
 
-  if (!rental)
-    throw createError.NotFound(
-      "Sorry!, Rental Process did not created before."
-    );
+  if (!rental) throw createError.NotFound(returns_msgs.rental_not_created);
 
-  if (rental.dateReturned)
-    throw createError.BadRequest("Sorry!, Rental already processed.");
+  if (rental.dateReturned) throw createError.BadRequest(returns_msgs.processed);
 
   rental.dateReturned = new Date();
 
@@ -43,7 +29,7 @@ const setReturns = async (req, res) => {
     { new: true }
   );
 
-  res.status(200).send({ data: rental });
+  res.status(200).send({ message: returns_msgs.sucess, data: rental });
 };
 
 module.exports = { setReturns };
